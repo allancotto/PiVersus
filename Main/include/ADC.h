6a16efb19940de578c7c9d736d97fa230dd3c2ce
Himikid <2265799j@student.gla.ac.uk>
2021-03-16 21:05:42 +0000

    updated header for bcm2835

--- Main/include/ADC.h
+++ Main/include/ADC.h
@@ -1,19 +1,26 @@
 /* 
-Library for communication with MCP3008 ADC (adapted from github.com/nodesign/MCP3008)
-which is ported originally from python code written by Adafruit
+Library for communication with MCP3008 ADC 
+based of github.com/marcod234 - integrated mcp3008 and bcm2835
 */
 
 #ifndef ADC_h
 #define ADC_h
 
-class ADC
-{
-    public:
-        ADC(int clock, int mosi, int miso, int cs);     /* defines ADC pins */
-        int readADC(int input_num) ; /*defines function to read specified pin from SPI*/
+#include <bcm2835.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#define ADC_SPI_AUX 1  //SPI1
+
+class ADC {
+    public:
+        bool begin(uint8_t spiModule=ADC_SPI_AUX, uint8_t cs=0);
+        int readADC(uint8_t channel);
+        int readADCDifference(uint8_t differential);
     private:
-        int _clock, _mosi, _miso,_cs;
+    uint8_t spiModule;
+    uint8_t cs;
+    int SPIxADC(uint8_t channel,bool differential);
 };
 
 
---
+++ Main/src/ADC.cpp
@@ -0,0 +1,3 @@
+/*class setup for adc*/
+
+
