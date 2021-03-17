320ae042afb53f3623629fc7b921a0b31bb1fe1b
Beepister <paulabro@hotmail.gr>
2021-03-17 18:16:48 +0000

    Added window event handling capabilities

--- Main/src/main.cpp
+++ Main/src/main.cpp
@@ -5,18 +5,48 @@
 
 int main()
 {
-    std::cout << "Hello Easy C++ project!" << std::endl;
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    std::cout << "From the PiVersus team, hello world!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "PiVersus");      //Making the window and giving it a title
+    window.setVerticalSyncEnabled(true);                                //Turning on V-Sync
+    sf::CircleShape shape(150.f);                                       //Making a placeholder shape
+    shape.setFillColor(sf::Color::Green);                               //Defining it's fill colour
 
+    //Run the program while the window is open. This is the Main loop
     while (window.isOpen())
     {
+        // Check window events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch (event.type) 
+            {
+                // "Close" event, closes window
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+
+                // Keyboard Key Press event
+                case sf::Event::KeyPressed:
+                    // Lovely code block that runs through the keyboard keys maybe
+                    break;
+
+                // Mouse Button Press event
+                case sf::Event::MouseButtonPressed:
+                    switch (event.mouseButton.button)
+                    {
+
+                    case sf::Mouse::Left:
+                        // Left Click code block
+                        break;
+
+                    default:
+                        break;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
         }
 
         window.clear();
