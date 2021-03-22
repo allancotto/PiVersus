9021e11bc13afc86798315efcdecf6a52f54111d
Himikid <2265799j@student.gla.ac.uk>
2021-03-22 23:39:39 +0000

    latest updates now using wiringPi as to bcm8235

--- Main/src/main.cpp
+++ Main/src/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <stdio.h>
+#include <string.h>
+#include <wiringPi.h>
+#include <mcp3004.h>
+#include <wiringPiSPI.h>
+#include <unistd.h>
 
-
-
-
+#define BASE 100
+#define SPI_CHAN 0
 
 int main()
 {
-    
+    int i;
+    printf("WiringPiSPISetup RC=%d\n",wiringPiSPISetup(0,500000));
+    mcp3004Setup(BASE,SPI_CHAN);
     
     std::cout << "Hello Easy C++ project!" << std::endl;
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
@@ -26,6 +33,13 @@ int main()
         window.clear();
         window.draw(shape);
         window.display();
+
+        for(i=0;i<8;i++){
+            printf("Channel %d: value=%4d\n",i,analogRead(BASE+i));
+            printf("\n");
+            sleep(0.5);
+        }
+        sleep(2);
 
         
     }
