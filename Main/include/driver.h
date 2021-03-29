16d21590a35031e8b08935c65ea4880fe340407d
Himikid <2265799j@student.gla.ac.uk>
2021-03-29 18:48:46 +0100

    updated driver file

--- Main/include/driver.h
+++ Main/include/driver.h
@@ -4,9 +4,26 @@ class will contain function to read and trouble shoot all sensors i.e joystick, 
 #ifndef driver_h
 #define driver_h
 
+//for use with MCP3008 SPI
 #define BASE 100
 #define SPI_CHAN 0
 
+//for use with Accelerometer (FXOS8700) I2C
+#define FXOS8700CQ_SLAVE_ADDR 0x1f // found using i2cdetect -y 1 (from datasheet can be 1c,1d,1e or 1f)
+#define FXOS8700CQ_CTRL_REG1 0x2A
+
+//FXOS8700 registers for data (data is a 14 bit word, 8 in msb and 6 in lsb - lsb has to be shifted >> 2)
+#define OUT_x_MSB 0x01
+#define OUT_x_LSB 0x01
+#define OUT_Y_MSB 0x01
+#define OUT_Y_LSB 0x01
+#define OUT_Z_MSB 0x01
+#define OUT_Z_LSB 0x01
+
+//struct to hold acceleration values
+struct accel {
+    int x, y, z;
+};
 
 class Driver
 {
@@ -16,10 +33,20 @@ private:
     
 public:
     Driver(); 
+
+    //MCP3008 function to read an ADC channel
     void printADChannel(int channel);
+
+    //joystick functions
     void printJoystickStatus(void);
     int getJoystickLR(void);
     int getJoystickUD(void);
+
+    //accelerometer functions
+    accel getAccelValues(void);
+
+
+
     
 };
 
