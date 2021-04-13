1948597fc0b4d4e7e13a5a50e53f46004b47c50d
Himikid <2265799j@student.gla.ac.uk>
2021-04-13 21:19:09 +0100

    games can now be selected and leads to instruction screen

--- Main/include/threadManager.h
+++ Main/include/threadManager.h
@@ -5,21 +5,36 @@
 #ifndef threadManager_h
 #define threadManager_h
 
+//thread manager also controls the state machine
+
+//state mapping, 0 = menu, 1 = instructionsGame1, 2 = instructionsGame2, 3 = instructionsGame3
+        
+
+
 class ThreadManager {
-
-
 
 public:
     ThreadManager(Menu* menu, Driver* driver); 
+
+    int state; //allow thread manager to access the state
+
     void checkJoystick(); //function to check the joystick
-    void checkPushbuttonMenu(); // function to check pushbutton
+    void checkPushButtonMenu(); // function to check pushbutton
+
+
     void setAllFalse(); // sets all thread variables to false to allow them to self terminate
 
-    //threads
+    void checkPushButtonInstructions();
+    void checkJoystickInstructions();
+    void switchToInstructionMenu(); 
+
+    // menu threads
+    void launchMenuThreads();
     sf::Thread menuJoystickThread; //this thread will check for joystick inputs
     sf::Thread menuPushButtonThread; //this thread will check for pushbutton inputs
     
     bool menuOpen;
+    bool instructionsOpen;
 
 private:
     Menu* menu; //instance in the class will allow thread functions to access menu functions.
--- Main/src/threadManager.cpp
+++ Main/src/threadManager.cpp
@@ -4,12 +4,13 @@
 #include "menu.h"
 
 ThreadManager::ThreadManager(Menu* menu, Driver* driver)
-: menuJoystickThread(&ThreadManager::checkJoystick, this), menuPushButtonThread(&ThreadManager::checkPushbutton, this) {
+: menuJoystickThread(&ThreadManager::checkJoystick, this), menuPushButtonThread(&ThreadManager::checkPushButtonMenu, this) {
     
     std::cout << "Initialising thread manager";
     menuOpen = true;
     this->menu = menu;
     this->driver = driver; 
+    state = 0; //initially set state to 0 for menu
     
 
 }
@@ -31,21 +32,25 @@ void ThreadManager::checkJoystick() {
     }
 }
 
-void ThreadManager::checkPushbuttonMenu() {
+void ThreadManager::checkPushButtonMenu() {
 
     while (menuOpen)
     {
         if(driver->getPushButton() == 1) {
+            menuOpen = false; // end menu threads
             switch (menu->menuState)
             {
             case 0:
                 std::cout << "Game 1 selected" << std::endl;
+                state = 1;
                 break;
             case 1:
                 std::cout << "Game 2 selected" << std::endl;
+                state = 2;
                 break;
 
             case 2:
+                state = 3;
                 std::cout << "Game 3 selected" << std::endl;
                 break;
             }
@@ -59,3 +64,12 @@ void ThreadManager::checkPushbuttonMenu() {
 void ThreadManager::setAllFalse() {
     menuOpen = false;
 }
+
+void ThreadManager::launchMenuThreads() {
+
+    menuJoystickThread.launch();
+    menuJoystickThread.launch();
+    
+}
+
+
